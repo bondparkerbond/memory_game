63d3bacb3f6d5c607aa1bf0cf9137652fbaf91bc
bondparkerbond <parkerbond@gmail.com>
2016-02-24 19:51:35 -0700

    cleaning up random array code

--- memory.js
+++ memory.js
@@ -7,14 +7,11 @@ $(document).ready(function(){
    */
   function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex ;
-
     // While there remain elements to shuffle...
     while (0 !== currentIndex) {
-
       // Pick a remaining element...
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex -= 1;
-
       // And swap it with the current element.
       temporaryValue = array[currentIndex];
       array[currentIndex] = array[randomIndex];
@@ -27,24 +24,10 @@ $(document).ready(function(){
   randarr = shuffle(arr);
   console.log(arr);
 
-
-  $('#front1:first-child').html("<h4 class='center-align'>" + randarr[0] + "</h4>");
-  $('#front2:first-child').html("<h4 class='center-align'>" + randarr[1] + "</h4>");
-  $('#front3:first-child').html("<h4 class='center-align'>" + randarr[2] + "</h4>");
-  $('#front4:first-child').html("<h4 class='center-align'>" + randarr[3] + "</h4>");
-  $('#front5:first-child').html("<h4 class='center-align'>" + randarr[4] + "</h4>");
-  $('#front6:first-child').html("<h4 class='center-align'>" + randarr[5] + "</h4>");
-  $('#front7:first-child').html("<h4 class='center-align'>" + randarr[6] + "</h4>");
-  $('#front8:first-child').html("<h4 class='center-align'>" + randarr[7] + "</h4>");
-  $('#front9:first-child').html("<h4 class='center-align'>" + randarr[8] + "</h4>");
-  $('#front10:first-child').html("<h4 class='center-align'>" + randarr[9] + "</h4>");
-  $('#front11:first-child').html("<h4 class='center-align'>" + randarr[10] + "</h4>");
-  $('#front12:first-child').html("<h4 class='center-align'>" + randarr[11] + "</h4>");
-  $('#front13:first-child').html("<h4 class='center-align'>" + randarr[12] + "</h4>");
-  $('#front14:first-child').html("<h4 class='center-align'>" + randarr[13] + "</h4>");
-  $('#front15:first-child').html("<h4 class='center-align'>" + randarr[14] + "</h4>");
-  $('#front16:first-child').html("<h4 class='center-align'>" + randarr[15] + "</h4>");
-
+  for (var i = 0; i < arr.length; i++){
+    var el = '#front' + (i+1).toString() + ':first-child'
+    $(el).html("<h4 class='center-align'>" + randarr[i] + "</h4>");
+  }
 
   //onclick function add flip class and then check to see if cards are the same
   // function selectCard() {
@@ -71,6 +54,71 @@ $(document).ready(function(){
     }
     else{
       Materialize.toast('Not a match', 1000);
+      
+      $('#front1').click(function(){
+        $('#front1').hide();
+        $('#back1').show();
+      });
+      $('#front2').click(function(){
+        $('#front2').hide();
+        $('#back2').show();
+      });
+      $('#front3').click(function(){
+        $('#front3').hide();
+        $('#back3').show();
+      });
+      $('#front4').click(function(){
+        $('#front4').hide();
+        $('#back4').show();
+      });
+      $('#front5').click(function(){
+        $('#front5').hide();
+        $('#back5').show();
+      });
+      $('#front6').click(function(){
+        $('#front6').hide();
+        $('#back6').show();
+      });
+      $('#front7').click(function(){
+        $('#front7').hide();
+        $('#back7').show();
+      });
+      $('#front8').click(function(){
+        $('#front8').hide();
+        $('#back8').show();
+      });
+      $('#front9').click(function(){
+        $('#front9').hide();
+        $('#back9').show();
+      });
+      $('#front10').click(function(){
+        $('#front10').hide();
+        $('#back10').show();
+      });
+      $('#front11').click(function(){
+        $('#front11').hide();
+        $('#back11').show();
+      });
+      $('#front12').click(function(){
+        $('#front12').hide();
+        $('#back12').show();
+      });
+      $('#front13').click(function(){
+        $('#front13').hide();
+        $('#back13').show();
+      });
+      $('#front14').click(function(){
+        $('#front14').hide();
+        $('#back14').show();
+      });
+      $('#front15').click(function(){
+        $('#front15').hide();
+        $('#back15').show();
+      });
+      $('#front16').click(function(){
+        $('#front16').hide();
+        $('#back16').show();
+      });
 
     }
     console.log(compareArray);
@@ -81,70 +129,6 @@ $(document).ready(function(){
   // Need to change below to not function on click
   // but to activate when two cards are not a pair
   // solves edge case of clicking on same card twice
-  $('#front1').click(function(){
-    $('#front1').hide();
-    $('#back1').show();
-  });
-  $('#front2').click(function(){
-    $('#front2').hide();
-    $('#back2').show();
-  });
-  $('#front3').click(function(){
-    $('#front3').hide();
-    $('#back3').show();
-  });
-  $('#front4').click(function(){
-    $('#front4').hide();
-    $('#back4').show();
-  });
-  $('#front5').click(function(){
-    $('#front5').hide();
-    $('#back5').show();
-  });
-  $('#front6').click(function(){
-    $('#front6').hide();
-    $('#back6').show();
-  });
-  $('#front7').click(function(){
-    $('#front7').hide();
-    $('#back7').show();
-  });
-  $('#front8').click(function(){
-    $('#front8').hide();
-    $('#back8').show();
-  });
-  $('#front9').click(function(){
-    $('#front9').hide();
-    $('#back9').show();
-  });
-  $('#front10').click(function(){
-    $('#front10').hide();
-    $('#back10').show();
-  });
-  $('#front11').click(function(){
-    $('#front11').hide();
-    $('#back11').show();
-  });
-  $('#front12').click(function(){
-    $('#front12').hide();
-    $('#back12').show();
-  });
-  $('#front13').click(function(){
-    $('#front13').hide();
-    $('#back13').show();
-  });
-  $('#front14').click(function(){
-    $('#front14').hide();
-    $('#back14').show();
-  });
-  $('#front15').click(function(){
-    $('#front15').hide();
-    $('#back15').show();
-  });
-  $('#front16').click(function(){
-    $('#front16').hide();
-    $('#back16').show();
-  });
 
   $('#back1').click(function(){
     var oneOne = $('#front1:first-child').text();
